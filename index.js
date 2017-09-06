502921e459984dcb91aa02191f7cba489d358317
srilq <git@srilq.email>
2017-09-06 01:25:21 +0100

    fixes, animation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var choo = require('choo')
+var html = require('choo/html')
 var css = require('sheetify')
+var animate = require('nanoanimation')
+require('web-animations-js')
 
 css('tachyons')
 
@@ -12,17 +15,31 @@ if (process.env.NODE_ENV === 'development') {
 }
 // app.use(require('choo-service-worker')())
 
-app.use(frogStore)
+app.use(frog)
 app.route('/', require('./views/main'))
 app.route('/*', require('./views/404'))
 
 if (!module.parent) app.mount('body')
 else module.exports = app
 
-function frogStore (state, emitter) {
+function frog (state, emitter) {
   state.frog = 'https://imgur.com/vVsjW9D.png'
-  emitter.on('frog', function (frog) {
+  state.boy = html`<img class="img w3 center db" alt="frog boy" src="/assets/frog.svg">`
+  state.spin = animate(state.boy, [
+    { transform: 'rotate(0deg)' },
+    { transform: 'rotate(360deg)' }
+  ], {
+    id: 'boy',
+    duration: 400,
+    iterations: Infinity,
+    fill: 'both'
+  })
+  emitter.on('frog', function (frog, boy) {
     state.frog = frog
+    state.spin.play()
     emitter.emit('render')
+  })
+  emitter.on('frogLoaded', function () {
+    state.spin.pause()
   })
 }
--- views/main.js
+++ views/main.js
@@ -1,7 +1,6 @@
 var html = require('choo/html')
 var path = require('path')
 var fs = require('fs')
-
 require('setimmediate')
 var randomFrog = require('random-frog')
 
@@ -13,13 +12,18 @@ function view (state, emit) {
   if (state.title !== title) emit(state.events.DOMTITLECHANGE, title)
   if (!state.frog || typeof state.frog !== 'string') state.frog = ''
 
+  var realBoy = html`<img class="img mv4 center db" alt="real frog boy" src="${state.frog}">`
+  realBoy.addEventListener('load', function () {
+    emit('frogLoaded')
+  }, false)
+
   return html`
     <body class="bg-black-90 white serif">
       <h1 class="f1 i tc">F R O G S</h1>
       <div class="pointer" onclick=${onclick}>
-        <img class="img w3 center db" alt="frog boy" src="/assets/frog.svg">
+        ${state.boy}
         <div class="f3 i tc">click me!</div>
-        <img class="img mv4 center db" alt="real frog boy" src="${state.frog}">
+        ${realBoy}
       </div>
       <p class="f5 i tc mt5 mb6">${fs.readFileSync(path.join(__dirname, '../assets/madefor.txt'), 'utf8')}</p>
     </body>
