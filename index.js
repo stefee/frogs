17fd3fb230e2b284a348ccfe5966b2a445dc36f1
Stef <stef.jones@condenast.com>
2020-07-17 23:05:51 +0100

    fix: upgrade dependencies

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var html = require('choo/html')
 var css = require('sheetify')
 var xhr = require('request')
 var uniqueRandomArray = require('unique-random-array')
-var animate = require('nanoanimation')
+var animation = require('nanoanimation')
 require('web-animations-js') // Web Animations polyfill
 
 css('tachyons')
@@ -15,7 +15,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(require('choo-log')())
   app.use(require('choo-service-worker/clear')())
 }
-app.use(require('choo-service-worker')())
 
 app.use(frog)
 app.route('/', require('./views/main'))
@@ -25,18 +24,20 @@ app.route('/:hash/*', require('./views/404'))
 if (!module.parent) app.mount('body')
 else module.exports = app
 
+const animateBoy = animation([
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+  id: 'spin',
+  duration: 400,
+  iterations: Infinity,
+  fill: 'both'
+})
+
 function frog (state, emitter) {
   state.boy = {}
   state.boy.el = html`<img class="img w3 center db" alt="spinny boy" src="/assets/frog.svg">`
-  state.boy.anim = animate(state.boy.el, [
-    { transform: 'rotate(0deg)' },
-    { transform: 'rotate(360deg)' }
-  ], {
-    id: 'spin',
-    duration: 400,
-    iterations: Infinity,
-    fill: 'both'
-  })
+  state.boy.anim = animateBoy(state.boy.el)
 
   state.frog = {
     current: null,
